8a1aed1448a84facc658d7be888d71b7bd6637ff
kobipro <Boatengkwabenaakuamoah@gmail.com>
2024-05-09 13:59:59 +0000

    fix the error in the whatsapptsx file

--- app/whatsappicon.tsx
+++ app/whatsappicon.tsx
@@ -1,16 +1,17 @@
-
-
 import React from 'react';
 import { AiOutlineWhatsApp } from 'react-icons/ai';
 
-const WhatsAppIcon = ({ link }) => {
+interface WhatsAppIconProps {
+  link: string;
+}
+
+const WhatsAppIcon: React.FC<WhatsAppIconProps> = ({ link }) => {
   return (
     <a
       href={link}
       target="_blank"
       rel="noopener noreferrer"
       className="bg-green-500 p-4 rounded-full shadow-lg fixed bottom-5 right-5 z-50"
-      // style={{ fontSize: '10rem' }} // Increase icon size
     >
       <AiOutlineWhatsApp className="text-white text-4xl" />
     </a>
@@ -18,5 +19,25 @@ const WhatsAppIcon = ({ link }) => {
 };
 
 export default WhatsAppIcon;
+
+
+// import React from 'react';
+// import { AiOutlineWhatsApp } from 'react-icons/ai';
+
+// const WhatsAppIcon = ({ link }) => {
+//   return (
+//     <a
+//       href={link}
+//       target="_blank"
+//       rel="noopener noreferrer"
+//       className="bg-green-500 p-4 rounded-full shadow-lg fixed bottom-5 right-5 z-50"
+//       // style={{ fontSize: '10rem' }} // Increase icon size
+//     >
+//       <AiOutlineWhatsApp className="text-white text-4xl" />
+//     </a>
+//   );
+// };
+
+// export default WhatsAppIcon;
 
 
